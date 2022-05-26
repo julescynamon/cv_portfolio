4b45cd53aeacd9cb0eb9d44a47694d7e75d34b9d
cynamon jules <julescweb@mail.com>
2022-05-26 16:46:52 +0200

    petite modif

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -7,63 +7,55 @@ import Projet4 from '../../assets/projet4.png';
 import Projet5 from '../../assets/projet5.png';
 import Projet6 from '../../assets/projet6.png';
 import Projet7 from '../../assets/projet7.png';
-// import Swiper core and required modules
-import {  Pagination } from 'swiper';
-
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
 
 
 const data = [
 	{
 		id: 1,
 		image: Projet1,
-		title: "Projet 2 formation OpenClassrooms",
+		title: "Projet 2 de la formation Openclassrooms",
 		github: "https://github.com/julescynamon/BoookiProject.github.io",
 		demo: "https://julescynamon.github.io/BoookiProject.github.io/",
 	},
 	{
 		id: 2,
 		image: Projet2,
-		title: "Projet 3 formation OpenClassrooms",
+		title: "Projet 3 de la formation Openclassrooms",
 		github: "https://github.com/julescynamon/CYNAMONJules_3_19102021",
 		demo: " https://julescynamon.github.io/CYNAMONJules_3_19102021/",
 	},
 	{
 		id: 3,
 		image: Projet3,
-		title: "Projet personel d'un site d'annonce ",
+		title: "Projet personel de Site d'annonce",
 		github: "https://github.com/julescynamon/siteD-annonce",
 		demo: " https://julescynamon.github.io/siteD-annonce/",
 	},
 	{
 		id: 4,
 		image: Projet4,
-		title: "Projet personel Site d'annonces immobilières",
+		title: "Projet personel de site d'annonces immobilières",
 		github: "https://github.com/julescynamon/AgenceImmobili-re",
 		demo: "https://julescynamon.github.io/AgenceImmobili-re/",
 	},
 	{
 		id: 5,
 		image: Projet5,
-		title: "Projet personel Site pour une salle de sport",
+		title: "Projet personel de site pour une salle de sport",
 		github: "https://github.com/julescynamon/Site-de-salle-de-sport",
 		demo: "https://julescynamon.github.io/Site-de-salle-de-sport/",
 	},
 	{
 		id: 6,
 		image: Projet6,
-		title: "Projet personel de Visualiseur audio en JS",
+		title: "Projet personel de visualiseur audio en JS",
 		github: "https://github.com/julescynamon/visualiseurAudio",
 		demo: "https://julescynamon.github.io/visualiseurAudio/",
 	},
 	{
 		id: 7,
 		image: Projet7,
-		title: "Projet personel du jeux Space Invader",
+		title: "Projet personel du Jeux Space Invader",
 		github: "https://github.com/julescynamon/SpaceInvaders",
 		demo: "https://julescynamon.github.io/SpaceInvaders/",
 	},
@@ -75,24 +67,19 @@ const Portfolio = () => {
 			<h5>Mon Travails</h5>
 			<h2>Portfolio</h2>
 
-			<Swiper
-				modules={[Pagination]}
-				spaceBetween={40}
-				slidesPerView={3}
-				navigation
-				pagination={{ clickable: true }}
-				className='container portfolio__container'
-			>
+			<div className='container portfolio__container'>
 				{data.map(({ id, image, title, github, demo }) => {
 					return (
-						<SwiperSlide key={id} className='portfolio__item'>
+						<article key={id} className='portfolio__item'>
 							<div className='portfolio__item-image'>
 								<img src={image} alt='' />
 							</div>
-							<h3>{title}</h3>
+							<h3>
+								{title}
+							</h3>
 							<div className='portfolio__item-cta'>
 								<a
-									href={github}
+									href= {github}
 									className='btn'
 									target='_blank'
 									rel='noreferrer'
@@ -100,7 +87,7 @@ const Portfolio = () => {
 									Github
 								</a>
 								<a
-									href={demo}
+									href= {demo}
 									className='btn btn-primary'
 									target='_blank'
 									rel='noreferrer'
@@ -108,10 +95,10 @@ const Portfolio = () => {
 									Live Demo
 								</a>
 							</div>
-						</SwiperSlide>
+						</article>
 					);
 				})}
-			</Swiper>
+			</div>
 		</section>
 	);
 }
