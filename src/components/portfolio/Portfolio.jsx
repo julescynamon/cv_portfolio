21bb81003b47dbdb785d165d89da8922e5c7a139
cynamon jules <julescweb@mail.com>
2022-09-05 10:24:35 +0200

    petit refactoring

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,23 +2,7 @@ import React, { useState } from "react";
 import "./portfolio.css";
 import { Waypoint } from "react-waypoint";
 import { useSpring, animated } from "react-spring";
-import Projet1 from "../../assets/projet1.png";
-import Projet2 from "../../assets/projet2.png";
-import Projet3 from "../../assets/projet3.png";
-import Projet4 from "../../assets/projet4.png";
-import Projet5 from "../../assets/projet5.png";
-import Projet6 from "../../assets/projet6.png";
-import Projet7 from "../../assets/projet7.png";
-import Projet8 from "../../assets/projet8.png";
-import Projet9 from "../../assets/projet9.png";
-import Projet10 from "../../assets/projet10.png";
-import Projet11 from "../../assets/projet11.png";
-import Projet12 from "../../assets/projet12.png";
-import Projet13 from "../../assets/projet13.png";
-import Projet14 from "../../assets/projet14.png";
-import Projet15 from "../../assets/projet15.png";
-import Projet16 from "../../assets/projet16.png";
-import Projet17 from "../../assets/projet17.png";
+
 // import Swiper core and required modules
 import { Navigation, Pagination } from "swiper";
 
@@ -29,128 +13,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-const data = [
-	{
-		id: 1,
-		image: Projet1,
-		title: "Intégrer une maquette en HTML et CSS",
-		github: "https://github.com/julescynamon/BoookiProject.github.io",
-		demo: "https://julescynamon.github.io/BoookiProject.github.io/",
-	},
-	{
-		id: 2,
-		image: Projet2,
-		title: "Dynamiser un site avec les animations SCSS",
-		github: "https://github.com/julescynamon/CYNAMONJules_3_19102021",
-		demo: " https://julescynamon.github.io/CYNAMONJules_3_19102021/",
-	},
-	{
-		id: 3,
-		image: Projet3,
-		title: "Projet personel du jeux du morpion en JS",
-		github: "https://github.com/julescynamon/JeuDuMorpion",
-		demo: " https://julescynamon.github.io/JeuDuMorpion/",
-	},
-	{
-		id: 4,
-		image: Projet4,
-		title: "Projet personel de site d'annonces immobilières",
-		github: "https://github.com/julescynamon/AgenceImmobili-re",
-		demo: "https://julescynamon.github.io/AgenceImmobili-re/",
-	},
-	{
-		id: 5,
-		image: Projet5,
-		title: "Projet personel de site pour une salle de sport",
-		github: "https://github.com/julescynamon/Site-de-salle-de-sport",
-		demo: "https://julescynamon.github.io/Site-de-salle-de-sport/",
-	},
-	{
-		id: 6,
-		image: Projet6,
-		title: "Projet personel de visualiseur audio en JS",
-		github: "https://github.com/julescynamon/visualiseurAudio",
-		demo: "https://julescynamon.github.io/visualiseurAudio/",
-	},
-	{
-		id: 7,
-		image: Projet7,
-		title: "Projet personel du Jeux Space Invader en JS",
-		github: "https://github.com/julescynamon/SpaceInvaders",
-		demo: "https://julescynamon.github.io/SpaceInvaders/",
-	},
-	{
-		id: 8,
-		image: Projet8,
-		title: "Projet exercice site E-Commerce en React",
-		github: "https://github.com/julescynamon/Ecommerce-react",
-		demo: "https://julescynamon.github.io/Ecommerce-react/",
-	},
-	{
-		id: 9,
-		image: Projet9,
-		title: "Projet exercice Pokédex en JS",
-		github: "https://github.com/julescynamon/pokedexjs",
-		demo: "https://julescynamon.github.io/pokedexjs/",
-	},
-	{
-		id: 10,
-		image: Projet10,
-		title: "Créer le Front d'un site E-commerce en Javascript",
-		github: "https://github.com/julescynamon/projetkanap",
-		demo: "https://projetkanap.netlify.app",
-	},
-	{
-		id: 11,
-		image: Projet11,
-		title: "Création d'un site pour un client Dj",
-		github: "https://github.com/julescynamon/dj-ms-site",
-		demo: "https://dj-ms-site.vercel.app/",
-	},
-	{
-		id: 12,
-		image: Projet12,
-		title: "Challenge FrontEnd Mentor, intégrer une maquette en js",
-		github: "https://github.com/julescynamon/frontEndChallenge1",
-		demo: "https://julescynamon.github.io/frontEndChallenge1/",
-	},
-	{
-		id: 13,
-		image: Projet13,
-		title: "Challenge FrontEnd Mentor, intégrer un bar-chart en js",
-		github: "https://github.com/julescynamon/frontEnd-challeng-bar-chart",
-		demo: "https://julescynamon.github.io/frontEnd-challeng-bar-chart/",
-	},
-	{
-		id: 14,
-		image: Projet14,
-		title: "Challenge FrontEnd Mentor, Advice generator app",
-		github: "https://github.com/julescynamon/frontEndChallengeAPI",
-		demo: "https://julescynamon.github.io/frontEndChallengeAPI/",
-	},
-	{
-		id: 15,
-		image: Projet15,
-		title: "Challenge FrontEnd Mentor, site réaliser en React",
-		github: "https://github.com/julescynamon/space_tourism",
-		demo: "https://space-tourism-ashy-six.vercel.app",
-	},
-	{
-		id: 16,
-		image: Projet16,
-		title: "Challenge FrontEnd Mentor, réaliser avec React Hook Form",
-		github: "https://github.com/julescynamon/frontend-mentor-formcards",
-		demo: "https://frontend-mentor-formcards.vercel.app",
-	},
-	{
-		id: 17,
-		image: Projet17,
-		title: "Challenge FrontEnd Mentor,Page E-commerce en React",
-		github: "https://github.com/julescynamon/EcommerceFrontEndMentor",
-		demo: "https://ecommerce-front-end-mentor.vercel.app",
-	},
-
-];
+import data from "./data";
 
 const Portfolio = () => {
 	const [toggleTxt, setToggleTxt] = useState(false);
