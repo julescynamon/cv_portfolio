3be78fd9a9567a49d29bb235af45426ae96fce78
cynamon jules <julescweb@mail.com>
2022-09-05 10:21:41 +0200

    rajout de nouveaux projets

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -12,6 +12,13 @@ import Projet7 from "../../assets/projet7.png";
 import Projet8 from "../../assets/projet8.png";
 import Projet9 from "../../assets/projet9.png";
 import Projet10 from "../../assets/projet10.png";
+import Projet11 from "../../assets/projet11.png";
+import Projet12 from "../../assets/projet12.png";
+import Projet13 from "../../assets/projet13.png";
+import Projet14 from "../../assets/projet14.png";
+import Projet15 from "../../assets/projet15.png";
+import Projet16 from "../../assets/projet16.png";
+import Projet17 from "../../assets/projet17.png";
 // import Swiper core and required modules
 import { Navigation, Pagination } from "swiper";
 
@@ -93,6 +100,56 @@ const data = [
 		github: "https://github.com/julescynamon/projetkanap",
 		demo: "https://projetkanap.netlify.app",
 	},
+	{
+		id: 11,
+		image: Projet11,
+		title: "Création d'un site pour un client Dj",
+		github: "https://github.com/julescynamon/dj-ms-site",
+		demo: "https://dj-ms-site.vercel.app/",
+	},
+	{
+		id: 12,
+		image: Projet12,
+		title: "Challenge FrontEnd Mentor, intégrer une maquette en js",
+		github: "https://github.com/julescynamon/frontEndChallenge1",
+		demo: "https://julescynamon.github.io/frontEndChallenge1/",
+	},
+	{
+		id: 13,
+		image: Projet13,
+		title: "Challenge FrontEnd Mentor, intégrer un bar-chart en js",
+		github: "https://github.com/julescynamon/frontEnd-challeng-bar-chart",
+		demo: "https://julescynamon.github.io/frontEnd-challeng-bar-chart/",
+	},
+	{
+		id: 14,
+		image: Projet14,
+		title: "Challenge FrontEnd Mentor, Advice generator app",
+		github: "https://github.com/julescynamon/frontEndChallengeAPI",
+		demo: "https://julescynamon.github.io/frontEndChallengeAPI/",
+	},
+	{
+		id: 15,
+		image: Projet15,
+		title: "Challenge FrontEnd Mentor, site réaliser en React",
+		github: "https://github.com/julescynamon/space_tourism",
+		demo: "https://space-tourism-ashy-six.vercel.app",
+	},
+	{
+		id: 16,
+		image: Projet16,
+		title: "Challenge FrontEnd Mentor, réaliser avec React Hook Form",
+		github: "https://github.com/julescynamon/frontend-mentor-formcards",
+		demo: "https://frontend-mentor-formcards.vercel.app",
+	},
+	{
+		id: 17,
+		image: Projet17,
+		title: "Challenge FrontEnd Mentor,Page E-commerce en React",
+		github: "https://github.com/julescynamon/EcommerceFrontEndMentor",
+		demo: "https://ecommerce-front-end-mentor.vercel.app",
+	},
+
 ];
 
 const Portfolio = () => {
