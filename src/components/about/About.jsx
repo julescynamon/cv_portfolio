6c31c613e5a1728151eb5c62be443dbd13b39e13
jules cynamon <julescweb@gmail.com>
2023-01-12 14:31:35 +0100

    update

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,56 +6,54 @@ import { VscFolderLibrary } from "react-icons/vsc";
 import { AiFillSchedule } from "react-icons/ai";
 
 const About = () => {
-	return (
-		<section id='about'>
-			<h2>À propos de moi</h2>
+  return (
+    <section id="about">
+      <h2>À propos de moi</h2>
 
-			<div className='container about__container about'>
-				<div className='about__me'>
-					<div className='about__me-image'>
-						<img src={ImgCode} alt='ecran avec du code' />
-					</div>
-				</div>
+      <div className="container about__container about">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ImgCode} alt="ecran avec du code" />
+          </div>
+        </div>
 
-				<div className='about__content'>
-					<div className='about__cards'>
-						<article className='about__card'>
-							<FaAward className='about__icon' />
-							<h5>Experience</h5>
-							<small>
-								1 ans de formation chez OpenClassrooms
-							</small>
-						</article>
-						<a href='#portfolio' className='lienXP'>
-							<article className='about__card'>
-								<VscFolderLibrary className='about__icon' />
-								<h5>Projets</h5>
-								<small>
-									Plus de 40 projets persos et école
-								</small>
-							</article>
-						</a>
-						<article className='about__card'>
-							<AiFillSchedule className='about__icon' />
-							<h5>Soft Kills</h5>
-							<small>
-								Adaptabilité, rigoureux, curieux, motivé{" "}
-							</small>
-						</article>
-					</div>
-					<p>
-						Je me présente Jules Cynamon, j'ai 33 ans, en
-						reconversion professionel, je viens d'obtenir mon
-						diplôme de web développeur chez OpenClassrooms. Ayant trouvé ma passion, je souhaiterais trouver une équipe avec qui m'épanouir et monter en compétences.
-						Ma facilité d'apprentissage, mon adaptabilité et ma persévérance sont des atouts, qui je pense, me permettraient d'exceller dans mon métier.  
-					</p>
-					<a href='#contact' className='btn btn-primary'>
-						Contactez-moi !
-					</a>
-				</div>
-			</div>
-		</section>
-	);
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className="about__icon" />
+              <h5>Experiences</h5>
+              <small>- 1 ans de formation chez OpenClassrooms</small>
+              <br />
+              <small>- 2 projets en freelance pour des clients locaux</small>
+            </article>
+            <a href="#portfolio" className="lienXP">
+              <article className="about__card">
+                <VscFolderLibrary className="about__icon" />
+                <h5>Projets</h5>
+                <small>Plus de 60 projets persos et école</small>
+              </article>
+            </a>
+            <article className="about__card">
+              <AiFillSchedule className="about__icon" />
+              <h5>Soft Kills</h5>
+              <small>Adaptabilité, rigoureux, curieux, motivé </small>
+            </article>
+          </div>
+          <p>
+            Vous recherchez un développeur junior passionné et motivé pour
+            intégrer votre équipe ? Je développe principalement en ReactJS et
+            TypeScript, avec une forte inclination pour le clean code. Je suis
+            également passionné par l'expérience utilisateur, l'UX/UI et le
+            design. Je suis rapide à apprendre et dévoué à mes projets, et je
+            suis prêt à mettre mes compétences au service de votre entreprise.
+          </p>
+          <a href="#contact" className="btn btn-primary">
+            Contactez-moi !
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
--- src/components/experiences/Experiences.jsx
+++ src/components/experiences/Experiences.jsx
@@ -1,97 +1,97 @@
-import React from 'react';
-import './experiences.css';
-import { BsFillPatchCheckFill } from 'react-icons/bs'
+import React from "react";
+import "./experiences.css";
+import { BsFillPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
-	return (
-		<section id='experiences' className="experience">
-			<h2>Mes hards skills</h2>
+  return (
+    <section id="experiences" className="experience">
+      <h2>Mes Compétences</h2>
 
-			<div className='container experience__container'>
-				<div className='experience__frontend'>
-					<h3>Languages Front-End</h3>
-					<div className='experience__content'>
-						<article className='experience__details'>
-							<BsFillPatchCheckFill className='experience__details-icons' />
-							<div>
-								<h4>HTML</h4>
-								<small className='text-light'>Junior</small>
-							</div>
-						</article>
-						<article className='experience__details'>
-							<BsFillPatchCheckFill className='experience__details-icons' />
-							<div>
-								<h4>CSS</h4>
-								<small className='text-light'>Junior</small>
-							</div>
-						</article>
-						<article className='experience__details'>
-							<BsFillPatchCheckFill className='experience__details-icons' />
-							<div>
-								<h4>BOOTSTRAP</h4>
-								<small className='text-light'>Junior</small>
-							</div>
-						</article>
-						<article className='experience__details'>
-							<BsFillPatchCheckFill className='experience__details-icons' />
-							<div>
-								<h4>SASS</h4>
-								<small className='text-light'>Junior</small>
-							</div>
-						</article>
-						<article className='experience__details'>
-							<BsFillPatchCheckFill className='experience__details-icons' />
-							<div>
-								<h4>REACT</h4>
-								<small className='text-light'>Junior</small>
-							</div>
-						</article>
-						<article className='experience__details'>
-							<BsFillPatchCheckFill className='experience__details-icons' />
-							<div>
-								<h4>VUEJS</h4>
-								<small className='text-light'>Débutant</small>
-							</div>
-						</article>
-					</div>
-				</div>
-				{/* Fin du FrontEnd */}
-				<div className='experience__backend'>
-					<h3>Languages Back-End</h3>
-					<div className='experience__content'>
-						<article className='experience__details'>
-							<BsFillPatchCheckFill className='experience__details-icons' />
-							<div>
-								<h4>NODEJS</h4>
-								<small className='text-light'>Junior</small>
-							</div>
-						</article>
-						<article className='experience__details'>
-							<BsFillPatchCheckFill className='experience__details-icons' />
-							<div>
-								<h4>MONGODB</h4>
-								<small className='text-light'>Junior</small>
-							</div>
-						</article>
-						<article className='experience__details'>
-							<BsFillPatchCheckFill className='experience__details-icons' />
-							<div>
-								<h4>MYSQL</h4>
-								<small className='text-light'>Junior</small>
-							</div>
-						</article>
-						<article className='experience__details'>
-							<BsFillPatchCheckFill className='experience__details-icons' />
-							<div>
-								<h4>API REST</h4>
-								<small className='text-light'>Junior</small>
-							</div>
-						</article>
-					</div>
-				</div>
-			</div>
-		</section>
-	);
-}
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Languages Front-End</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icons" />
+              <div>
+                <h4>HTML</h4>
+                <small className="text-light">Junior</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icons" />
+              <div>
+                <h4>CSS</h4>
+                <small className="text-light">Junior</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icons" />
+              <div>
+                <h4>BOOTSTRAP</h4>
+                <small className="text-light">Junior</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icons" />
+              <div>
+                <h4>SASS</h4>
+                <small className="text-light">Junior</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icons" />
+              <div>
+                <h4>REACT</h4>
+                <small className="text-light">Junior</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icons" />
+              <div>
+                <h4>VUEJS</h4>
+                <small className="text-light">Débutant</small>
+              </div>
+            </article>
+          </div>
+        </div>
+        {/* Fin du FrontEnd */}
+        <div className="experience__backend">
+          <h3>Languages Back-End</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icons" />
+              <div>
+                <h4>NODEJS</h4>
+                <small className="text-light">Junior</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icons" />
+              <div>
+                <h4>MONGODB</h4>
+                <small className="text-light">Junior</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icons" />
+              <div>
+                <h4>MYSQL</h4>
+                <small className="text-light">Junior</small>
+              </div>
+            </article>
+            <article className="experience__details">
+              <BsFillPatchCheckFill className="experience__details-icons" />
+              <div>
+                <h4>API REST</h4>
+                <small className="text-light">Junior</small>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Experience
+export default Experience;
